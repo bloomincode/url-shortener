afa9e67e7bd030a3e3424d615b1046fc6bc9ae1b
Georgina Nuthall <georgina.nuthall@gmail.com>
2022-01-20 18:00:13 +0000

    remove token

--- url-shortener.py
+++ url-shortener.py
@@ -1,13 +1,13 @@
 import bitly_api
 
-BITLY_GENERIC_ACCESS_TOKEN = "dfeb0739f7c506dced3a4dc35baf94f446e82d6a"
+BITLY_GENERIC_ACCESS_TOKEN = "ENTER TOKEN"
 
 def main():
     bitly = bitly_api.Connection(access_token=BITLY_GENERIC_ACCESS_TOKEN)
     print("Enter URL :")
     full_link = input()
     short_url = bitly.shorten(full_link)
-    print("Short URL = %s" % short_url['url'])
+    print("Mini URL = %s" % short_url['url'])
 
 if __name__ == '__main__':
     main()
